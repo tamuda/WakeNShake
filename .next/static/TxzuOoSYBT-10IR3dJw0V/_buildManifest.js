a968657215c3432f27d5376d7156cd2fba8d1868
Tamuda <tchimhan@u.rochester.edu>
2023-05-06 08:21:23 +0200

    added team

--- .next/static/TxzuOoSYBT-10IR3dJw0V/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/103-a65f89803227ea73.js","static/chunks/pages/index-bb39b04147314cf6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -35,6 +35,7 @@ export default function Home() {
       <Pricing />
       <Waitlist />
       <Testimonials />
+      <Team />
 
       <Footer />
     </>
